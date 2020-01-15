db0baab5214a669a992835ab91be0bc24b85efb7
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2020-01-15 19:33:27 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,8 +4,7 @@ API_TOKEN = "923692974:AAFNwAZZuGojdgWu0jsCxb5qQ0EhobAnMMM"
 ADMIN_ID = 684842099
 
 
-HELLO_MESSAGE = "🔊 | Sad Station™\n━━━━━━━━━━━━━━\n\n
-🔊 Uma estação de músicas tristes, isso é @Sad_station.\n\n📁 Melhor acervo de Músicas Triste.\n\nSeu Canal de Música Sad no Telegram. #Compartilhe\n\n\n🎉Envie me Sua Mensagem" 
+HELLO_MESSAGE = "🔊 | Sad Station™\n━━━━━━━━━━━━━━\n\n🔊 Uma estação de músicas tristes, isso é @Sad_station.\n\n📁 Melhor acervo de Músicas Triste.\n\nSeu Canal de Música Sad no Telegram. #Compartilhe\n\n\n🎉Envie me Sua Mensagem" 
 NEW_FEEDBACK = "Alguém lhe enviou uma mensagem."
 YES = "Sim✅"
 NO = "Não🚫"
